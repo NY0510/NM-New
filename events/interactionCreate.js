47e21f58e6fd11c3ef9bfef99a5dd8863cd7a18d
NY64 <me@ny64.kr>
2023-10-05 23:29:34 +0900

    Add logging option

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -5,7 +5,9 @@ module.exports = {
 
 	async execute(interaction) {
 		if (interaction.isButton()) {
-			return log.info(`"${interaction.guild.name}" 서버에서 "${interaction.user.tag}" 유저가 "${interaction.customId}" 버튼을 "#${interaction.channel.name}" 채널에서 클릭했습니다`);
+			if (interaction.client.config.logging.button)
+				log.info(`"${interaction.guild.name}" 서버에서 "${interaction.user.tag}" 유저가 "${interaction.customId}" 버튼을 "#${interaction.channel.name}" 채널에서 클릭했습니다`);
+			return;
 		} else if (interaction.isChatInputCommand()) {
 			const command = interaction.client.commands.get(interaction.commandName);
 
@@ -15,7 +17,8 @@ module.exports = {
 
 			try {
 				await command.execute(interaction);
-				log.info(`"${interaction.guild.name}" 서버에서 "${interaction.user.tag}" 유저가 "${interaction.commandName}" 명령어를 "#${interaction.channel.name}" 채널에서 실행했습니다`);
+				if (interaction.client.config.logging.command)
+					log.info(`"${interaction.guild.name}" 서버에서 "${interaction.user.tag}" 유저가 "${interaction.commandName}" 명령어를 "#${interaction.channel.name}" 채널에서 실행했습니다`);
 			} catch (e) {
 				await interaction.followUp({ content: `\`${interaction.commandName}\` 명령어를 실행하는 중 오류가 발생했어요`, ephemeral: true });
 				log.error(`${interaction.commandName} 명령어를 실행하는 중 오류가 발생했습니다\nError: ${e}`);

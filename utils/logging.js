6ebc4ad119e627e02e7751144d653db9a5f24a09
NY64 <me@ny64.kr>
2023-10-05 23:19:31 +0900

    Add debug log function

--- utils/logging.js
+++ utils/logging.js
@@ -4,4 +4,5 @@ module.exports = {
 	info: (message) => console.log(`${chalk.blue("[INFO]")} ${message}`),
 	warn: (message) => console.log(`${chalk.yellow("[WARNING]")} ${message}`),
 	error: (message) => console.log(`${chalk.red("[ERROR]")} ${message}`),
+	debug: (message) => console.log(`${chalk.green("[DEBUG]")} ${message}`),
 };
